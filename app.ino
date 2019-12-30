6e0c7f50d510b3540d514c362fa4520393acf72d
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 01:41:09 +0200

    Set up screen

--- app.ino
+++ app.ino
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include "src/config.h"
+
+LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
 void setup()
 {
     Serial.begin(SERIAL_BAUDRATE);
     Serial.println("Hello world");
+
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+    lcd.print("Hello world.");
 }
 
 void loop()
--- src/config.h
+++ src/config.h
@@ -1,1 +1,8 @@
 #define SERIAL_BAUDRATE 9600
+
+/*
+ * LCD Display
+ */
+#define LCD_ADDRESS 0x3F
+#define LCD_COLUMNS 16
+#define LCD_ROWS 2
