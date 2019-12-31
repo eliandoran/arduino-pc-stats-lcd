ddbae2b73b0e3e14a95c49d3170d978263157bd7
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 17:12:56 +0200

    Display temperature placeholder

--- src/lcd.h
+++ src/lcd.h
@@ -21,6 +21,15 @@ void LCD_PrintCentered(const char* text, int y) {
     }
 }
 
+void LCD_PrintRight(const char* text, int y) {
+    int length = strlen(text);
+
+    if (length > 0) {
+        int x = (LCD_COLUMNS - length);
+        LCD_Print(text, x, y);
+    }
+}
+
 void LCD_Clear() {
     lcd.clear();
 }
--- src/screens/cpu-gpu.h
+++ src/screens/cpu-gpu.h
@@ -9,5 +9,8 @@ void Screen_CPU_GPU() {
     LCD_SetCustomCharacter(ICON_GPU, CHAR_GPU);
 
     LCD_DrawCustomCharacter(ICON_CPU, 0, 0);
-    LCD_DrawCustomCharacter(ICON_GPU, 0, 1);    
+    LCD_DrawCustomCharacter(ICON_GPU, 0, 1);
+
+    LCD_PrintRight("32", 0);
+    LCD_PrintRight("42", 1);
 }
