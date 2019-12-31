e2f1545758898685c48a8412c1fbad0f088fe4bc
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 23:05:38 +0200

    Draw partial segment in progress bar

--- src/ui/hprogress.h
+++ src/ui/hprogress.h
@@ -10,6 +10,15 @@
 #define INDEX_HPROGRESS_80  4
 #define INDEX_HPROGRESS_100 3
 
+const byte HPROGRESS_INTERMEDIATE_STEPS[] = {
+    INDEX_HPROGRESS_20,
+    INDEX_HPROGRESS_40,
+    INDEX_HPROGRESS_60,
+    INDEX_HPROGRESS_80
+};
+
+const byte HPROGRESS_NUM_INTERMEDIATE_STEPS = (sizeof(HPROGRESS_INTERMEDIATE_STEPS) / sizeof(HPROGRESS_INTERMEDIATE_STEPS[0]));
+
 void HProgress_Initialize() {
     LCD_SetCustomCharacter(INDEX_HPROGRESS_20,  CHAR_HPROGRESS_20);
     LCD_SetCustomCharacter(INDEX_HPROGRESS_40,  CHAR_HPROGRESS_40);
@@ -19,17 +28,20 @@ void HProgress_Initialize() {
 }
 
 void HProgress_Draw(int x, int y, int width, byte value) {
-    int numPixels = (((float)value / 255) * width * LCD_CHAR_WIDTH);
-    int numFullSegments = (numPixels / LCD_CHAR_WIDTH);
+    int numPixels = (((float)value / 255) * width * LCD_CHAR_WIDTH);    
     
-    TRACE_VAL(value);
-    TRACE_VAL(width);
-    TRACE_VAL(numPixels);
-    TRACE_VAL(numFullSegments);
-
+    // Draw full segments
+    int numFullSegments = (numPixels / LCD_CHAR_WIDTH);
     for (int i=0; i<numFullSegments; i++) {
         LCD_DrawCustomCharacter(INDEX_HPROGRESS_100, x, y);
         x++;
+    }
+
+    // Draw last partial segment.
+    int numPartialSegments = (numPixels % LCD_CHAR_WIDTH);
+    if (numPartialSegments > 0) {
+        int index = (numPartialSegments / HPROGRESS_NUM_INTERMEDIATE_STEPS);
+        LCD_DrawCustomCharacter(HPROGRESS_INTERMEDIATE_STEPS[index], x, y);
     }
 }
 
