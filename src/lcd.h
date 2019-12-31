dfc4e0d4201408f63e3907ea0c7f698ad37a1427
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 03:23:17 +0200

    Clear line when printing centered

--- src/lcd.h
+++ src/lcd.h
@@ -7,17 +7,21 @@
 
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
-void LCD_Initialize() {
-    lcd.init();
-    lcd.backlight();
-    lcd.clear();    
-
-    TRACE("[LCD] Initialized.");
-}
-
 void LCD_Print(const char* text, int x, int y) {
     lcd.setCursor(x, y);
     lcd.print(text);
+}
+
+void LCD_Clear() {
+    lcd.clear();
+}
+
+void LCD_ClearLine(int y) {
+    String emptyLine;
+    for (int i=0; i<LCD_COLUMNS; i++) {
+        emptyLine += ' ';
+    }
+    LCD_Print(emptyLine.c_str(), 0, y);
 }
 
 void LCD_PrintCentered(const char* text, int y) {
@@ -25,6 +29,15 @@ void LCD_PrintCentered(const char* text, int y) {
 
     if (length > 0) {
         int x = (LCD_COLUMNS / 2 - length / 2);    
+        LCD_ClearLine(y);
         LCD_Print(text, x, y);
     }
+}
+
+void LCD_Initialize() {
+    lcd.init();
+    lcd.backlight();
+    LCD_Clear();
+
+    TRACE("[LCD] Initialized.");
 }
