83021a3b4a0a37170b817d8d92551b029ed5be10
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 03:11:29 +0200

    Update date only once per day

--- src/screens/idle.h
+++ src/screens/idle.h
@@ -2,13 +2,21 @@
 #include "../rtc.h"
 #include "../util/datetime.h"
 
+DateTime lastTime = DateTime(0);
+
 void Screen_Idle() {        
     DateTime now = RTC_GetTime();
+
+    // Display time
     String timeFormatted = DateTime_FormatTime(now);
-    String dateFormatted = DateTime_FormatDate(now);
-
     LCD_PrintCentered(timeFormatted.c_str(), 0);
-    LCD_PrintCentered(dateFormatted.c_str(), 1);
 
+    // Display date
+    if (now.day() != lastTime.day()) {
+        String dateFormatted = DateTime_FormatDate(now);    
+        LCD_PrintCentered(dateFormatted.c_str(), 1);
+    }
+
+    lastTime = now;
     delay(1000);
 }
