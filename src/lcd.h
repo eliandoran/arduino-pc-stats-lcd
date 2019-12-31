4efd0d43e87c1d1366e653c39cf28126c989ead0
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 17:45:38 +0200

    Add print char

--- src/lcd.h
+++ src/lcd.h
@@ -12,6 +12,11 @@ void LCD_Print(const char* text, int x, int y) {
     lcd.print(text);
 }
 
+void LCD_Print(char ch, int x, int y) {
+    lcd.setCursor(x, y);
+    lcd.print(ch);
+}
+
 void LCD_PrintCentered(const char* text, int y) {
     int length = strlen(text);
 
@@ -46,7 +51,7 @@ void LCD_SetCustomCharacter(byte charIndex, uint8_t charData[]) {
     lcd.createChar(charIndex, charData);
 }
 
-void LCD_DrawCustomCharacter(byte charIndex, int x, int y) {
+void LCD_DrawCustomCharacter(int charIndex, int x, int y) {
     lcd.setCursor(x, y);
     lcd.write(charIndex);
 }
