d7eba8f3978b81f690d0fcb33ff9a5666af5d832
Elian Doran <doran.adoris.elian@gmail.com>
2019-12-31 23:21:47 +0200

    Display Celsius degree symbol

--- src/screens/cpu-gpu.h
+++ src/screens/cpu-gpu.h
@@ -5,17 +5,22 @@
 
 #define ICON_CPU 0
 #define ICON_GPU 1
+#define ICON_CELSIUS 2
+
+#define ICON_CELSIUS_STR "\x2"
 
 void Screen_CPU_GPU() {
     LCD_SetCustomCharacter(ICON_CPU, CHAR_CPU);
     LCD_SetCustomCharacter(ICON_GPU, CHAR_GPU);
+    LCD_SetCustomCharacter(ICON_CELSIUS, CHAR_CELSIUS_DEGREE);
 
     LCD_DrawCustomCharacter(ICON_CPU, 0, 0);
     LCD_DrawCustomCharacter(ICON_GPU, 0, 1);
 
-    LCD_PrintRight("32" CHAR_DEGREE_STR, 0);
-    LCD_PrintRight("42" CHAR_DEGREE_STR, 1);
+    LCD_PrintRight("32" ICON_CELSIUS_STR, 0);
+    LCD_PrintRight("42" ICON_CELSIUS_STR, 1);
     
+    HProgress_Initialize();
     HProgress_Draw(2, 0, 10, HProgress_GetValue(0, 50, 100));
     HProgress_Draw(2, 1, 10, HProgress_GetValue(0, 100, 100));
 }
