bff5f8b660a7003f637f2dee6e90e0e912263924
Elian Doran <doran.adoris.elian@gmail.com>
2020-01-01 23:38:55 +0200

    Parse first command over serial

--- src/command.cpp
+++ src/command.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
+#include <string.h>
 #include "util/trace.h"
+
+String serialBuffer;
+
+void Command_ParseExec(String command) {
+    TRACE_VAL(String("[COMMAND] Got command ") + command);
+}
 
 void Command_Check() {
     if (Serial.available() < 1)
         return;
 
-    String commandStr = Serial.readString();
-    TRACE_VAL(String("[COMMAND] Got command ") + commandStr);
+    String inputStr = Serial.readString();
+    serialBuffer += inputStr;
+    
+    int newLineIndex = serialBuffer.indexOf('\n');
+    if (newLineIndex > 0) {
+        String command = serialBuffer.substring(0, newLineIndex - 1);
+        Command_ParseExec(command);
+    } else {
+        TRACE("Did not get newline!")
+    }
 }
