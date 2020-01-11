bd0224ef1981ccbbb518a7c4eaa4b87f53fd36c6
Elian Doran <doran.adoris.elian@gmail.com>
2020-01-12 01:22:48 +0200

    Use a queue for sending commands

--- datasender/command.js
+++ datasender/command.js
@@ -1,16 +1,37 @@
+const FLUSH_INTERVAL = 10;
+
 class CommandSender {
     constructor(port) {
         this.port = port;
+        this.queue = [];
+        this.timer = null;
     }
 
-    sendCommand(command, ...args) {   
-        let output = [ command, ...args ].join(" ") + "\r\n";
-        console.log("send:", output);
+    queueCommand(command, ...args) {   
+        let output = [ command, ...args ].join(" ") + "\r\n";        
+        this.queue.push(output);
+        this.processQueue();
+    }
+
+    processQueue() {
+        if (this.timer === null) {
+            this.timer = setInterval(this.processQueueInterval.bind(this), FLUSH_INTERVAL);
+        }
+    }
+
+    processQueueInterval() {
+        let output = this.queue.shift();
+        console.log("send:", output);        
         this.port.write(output);
+
+        if (this.queue.length == 0) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
     }
     
     setRegister(id, value) {
-        this.sendCommand("s", id, value);
+        this.queueCommand("s", id, value);
     }
     
 }
