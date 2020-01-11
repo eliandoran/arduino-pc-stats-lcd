8c175fb19da4456bc3db636c31577d27b3bf4beb
Elian Doran <doran.adoris.elian@gmail.com>
2020-01-12 01:41:32 +0200

    Integrate data sender with data provider

--- datasender/index.js
+++ datasender/index.js
@@ -4,30 +4,35 @@ const Readline = require("@serialport/parser-readline");
 const map = require("./mapper");
 const CommandSender = require("./command");
 
-const parsedData = require("../provider-nzxtcam/parsed.json");
+class DataSender {
+    start(callback) {
+        console.time("connect");
 
-console.time("connect");
+        const port = new SerialPort("COM3", {
+            baudRate: 115200
+        });
+         
+        const parser = port.pipe(new Readline());
+        this.command = new CommandSender(port);
+        
+        parser.on("data", (data) => {
+            data = data.toString("utf8").trim();    
+            console.log("recv:", data);
+        
+            if (data == "Ready.") {
+                console.timeEnd("connect");
+                callback();
+            }
+        });
+    }
 
-const port = new SerialPort("COM3", {
-    baudRate: 115200
-});
- 
-const parser = port.pipe(new Readline());
-const command = new CommandSender(port);
-
-parser.on("data", (data) => {
-    data = data.toString("utf8").trim();    
-    console.log("recv:", data);
-
-    if (data == "Ready.") {
-        console.timeEnd("connect");        
-
-        const output = map(parsedData);
+    sendData(data) {
+        const output = map(data);
         for (const id of Object.keys(output)) {
             const value = output[id];
-            command.setRegister(id, value);
+            this.command.setRegister(id, value);
         }
-
-        console.log(output);
     }
-});
+}
+
+module.exports = DataSender;
--- provider-nzxtcam/src/monitor.js
+++ provider-nzxtcam/src/monitor.js
@@ -1,18 +1,25 @@
 const {monitor} = require('../dist/lib').getCore();
 const parse = require("./parser");
 
-function start() {
-    monitor.on('initialized', (refresh) => {
-        const pc = refresh.pc
-        console.log("hardware monitor loaded, initial values: ", pc)
-    })
+const DataSender = require("datasender");
 
-    monitor.on('refresh', (refresh) => {
-        const pc = refresh.pc
-        const data = parse(pc);
-        
-        require("fs").writeFile("D:\\Projects\\Arduino PC Stats LCD\\provider-nzxtcam\\data.json", JSON.stringify(pc, null, 4), () => {});
-        require("fs").writeFile("D:\\Projects\\Arduino PC Stats LCD\\provider-nzxtcam\\parsed.json", JSON.stringify(data, null, 4), () => {});
+function start() {
+    const sender = new DataSender();
+
+    sender.start(() => {
+        monitor.on('initialized', (refresh) => {
+            const pc = refresh.pc
+            console.log("hardware monitor loaded, initial values: ", pc)
+        })
+    
+        monitor.on('refresh', (refresh) => {
+            const pc = refresh.pc
+            const data = parse(pc);
+            
+            require("fs").writeFile("D:\\Projects\\Arduino PC Stats LCD\\provider-nzxtcam\\data.json", JSON.stringify(pc, null, 4), () => {});
+            require("fs").writeFile("D:\\Projects\\Arduino PC Stats LCD\\provider-nzxtcam\\parsed.json", JSON.stringify(data, null, 4), () => {});        
+            sender.sendData(data);
+        });
     });    
 }
 
