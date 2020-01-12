665071b3614bb799f94cc249d86ea0cfeb868df0
Elian Doran <doran.adoris.elian@gmail.com>
2020-01-12 02:43:55 +0200

    Improve temperature screen performance

--- arduino/src/screens/temperature.cpp
+++ arduino/src/screens/temperature.cpp
@@ -10,13 +10,21 @@
 #define ICON_MOTHERBOARD 2
 #define ICON_CELSIUS_DEGREE 3
 
-static void drawComponent(int icon, int registryIndex, int x, int width) {
+int lastValues[3] = { -1, -1, -1 };
+
+static void drawComponent(int icon, int registryIndex, int x, int width, bool initialized) {
     int temperature = Registry_GetValue(registryIndex);
     bool hasValue = Registry_IsSet(registryIndex);
     String temperatureStr = Locale_FormatTemperature(temperature, ICON_CELSIUS_DEGREE, hasValue);
 
+    if (initialized && temperature == lastValues[icon]) {       
+        return; 
+    }
+
     LCD_DrawCustomCharacter(icon, x + width / 2, 0);    
     LCD_PrintCentered(temperatureStr, x, 1, width);
+
+    lastValues[icon] = temperature;
 }
 
 void Screen_Temperature(bool initialized) {
@@ -29,7 +37,7 @@ void Screen_Temperature(bool initialized) {
 
     int width = LCD_COLUMNS / 3;
 
-    drawComponent(ICON_CPU, REGISTRY_TEMP_CPU, 0, width);
-    drawComponent(ICON_GPU, REGISTRY_TEMP_GPU, 6, width);
-    drawComponent(ICON_MOTHERBOARD, REGISTRY_TEMP_MOTHERBOARD, 12, width);    
+    drawComponent(ICON_CPU, REGISTRY_TEMP_CPU, 0, width, initialized);
+    drawComponent(ICON_GPU, REGISTRY_TEMP_GPU, 6, width, initialized);
+    drawComponent(ICON_MOTHERBOARD, REGISTRY_TEMP_MOTHERBOARD, 12, width, initialized);
 }
